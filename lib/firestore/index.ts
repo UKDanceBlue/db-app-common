8b172b13340685773a88d92ac3c65b0b8c6a268b
Tag Howard <tag.howard1080@gmail.com>
2022-10-31 12:15:15 -0400

    Add RecursivePartial type

--- lib/firestore/index.ts
+++ lib/firestore/index.ts
@@ -1,4 +1,4 @@
 export * from "./FirestoreUser";
-export * from "./FirestoreImage"
-export * from "./FirestoreSponsor"
-export * from "./FirestoreTeam"
+export * from "./FirestoreImage";
+export * from "./FirestoreSponsor";
+export * from "./FirestoreTeam";
