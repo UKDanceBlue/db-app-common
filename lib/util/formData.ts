ac30fc225754e36dd67ee24762b0b5d6d68d676c
Tag Howard <tag.howard1080@gmail.com>
2023-05-28 13:08:36 -0400

    Remove file support from 'formDataToJson'

--- lib/util/formData.ts
+++ lib/util/formData.ts
@@ -1,53 +1,30 @@
 import type { Primitive } from "./TypeUtils.js";
 
-export function formDataToJson<AllowFiles extends boolean = false>(
+export function formDataToJson(
   formData: FormData,
-  allowFiles: AllowFiles = false as AllowFiles
-): Record<
-  string,
-  Primitive | Primitive[] | (AllowFiles extends true ? File : never)
-> {
-  if (allowFiles) {
-    const json: Record<string, Primitive | Primitive[] | File> = {};
+  omitEmptyValues = true
+): Record<string, Primitive | Primitive[]> {
+  const json: Record<string, Primitive | Primitive[]> = {};
 
-    for (const [key, value] of formData.entries()) {
-      if (!Reflect.has(json, key)) {
-        json[key] = value;
-        continue;
-      }
-
-      if (!Array.isArray(json[key])) {
-        if (json[key] instanceof Blob) {
-          throw new TypeError("Cannot have multiple files with the same key.");
-        }
-        json[key] = [json[key] as Primitive];
-      }
-      (json[key] as Primitive[]).push(value as Primitive);
+  for (const [key, value] of formData.entries()) {
+    if (omitEmptyValues && value === "") {
+      continue;
     }
 
-    return json as Record<
-      string,
-      Primitive | Primitive[] | (AllowFiles extends true ? File : never)
-    >;
-  } else {
-    const json: Record<string, Primitive | Primitive[]> = {};
-
-    for (const [key, value] of formData.entries()) {
-      if (value instanceof Blob) {
-        throw new TypeError("Files are not allowed.");
-      }
-
-      if (!Reflect.has(json, key)) {
-        json[key] = value;
-        continue;
-      }
-
-      if (!Array.isArray(json[key])) {
-        json[key] = [json[key] as Primitive];
-      }
-      (json[key] as Primitive[]).push(value);
+    if (value instanceof Blob) {
+      throw new TypeError("Files are not supported by JSON.");
     }
 
-    return json;
+    if (!Reflect.has(json, key)) {
+      json[key] = value;
+      continue;
+    }
+
+    if (!Array.isArray(json[key])) {
+      json[key] = [json[key] as Primitive];
+    }
+    (json[key] as Primitive[]).push(value);
   }
+
+  return json;
 }
