119d33cfd0178d822e21406d634cf05892e94d17
Tag Howard <tag.howard1080@gmail.com>
2023-05-27 21:29:40 -0400

    Fix index

--- lib/index.ts
+++ lib/index.ts
@@ -1,55 +1,36 @@
 import "reflect-metadata";
 
-export * from "./util/TypeUtils.js";
 export * from "./api/request/BodyTypes.js";
 export * from "./api/request/ListQueryTypes.js";
 export * from "./api/request/SearchTypes.js";
 export * from "./api/request/Event.js";
-export * from "./auth/index.js";
-
-export * from "./util/comparators.js";
 
 export * from "./api/response/JsonResponse.js";
 
-export { Resource as ApiResource } from "./api/resource/Resource.js";
-export {
-  ConfigurationResource,
-  ConfigurationResourceInitializer,
-} from "./api/resource/Configuration.js";
-export {
-  EventResource,
-  EventResourceInitializer,
-} from "./api/resource/Event.js";
-export {
-  ImageResource,
-  ImageResourceInitializer,
-} from "./api/resource/Image.js";
-export {
-  LoginFlowSessionResource,
-  LoginFlowSessionResourceInitializer,
-} from "./api/resource/LoginFlowSession.js";
+export * from "./api/resource/Resource.js";
+export * from "./api/resource/Configuration.js";
+export * from "./api/resource/Event.js";
+export * from "./api/resource/Image.js";
+export * from "./api/resource/LoginFlowSession.js";
 export {
   NotificationResource,
   NotificationResourceInitializer,
 } from "./api/resource/Notification.js";
-export {
-  PersonResource,
-  PersonResourceInitializer,
-} from "./api/resource/Person.js";
-export {
-  PointEntryResource,
-  PointEntryResourceInitializer,
-} from "./api/resource/PointEntry.js";
-export {
-  PointOpportunityResource,
-  PointOpportunityResourceInitializer,
-} from "./api/resource/PointOpportunity.js";
-export { RoleResource, RoleResourceInitializer } from "./api/resource/Role.js";
-export {
-  TeamResource,
-  TeamType,
-  TeamResourceInitializer,
-} from "./api/resource/Team.js";
+export * from "./api/resource/Person.js";
+export * from "./api/resource/PointEntry.js";
+export * from "./api/resource/PointOpportunity.js";
+export * from "./api/resource/Role.js";
+export * from "./api/resource/Team.js";
+
+export * from "./auth/index.js";
+export * from "./auth/role.js";
+
+export * from "./util/base64.js";
+export * from "./util/comparators.js";
+export * from "./util/formData.js";
+export * from "./util/intervalTools.js";
+export * from "./util/TypeUtils.js";
+export * from "./util/resourceValidation.js";
 
 export { ApiClient } from "./client/ApiClient.js";
 
